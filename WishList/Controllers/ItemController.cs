ce86a006ab4bc7965ab43d7946a895e15442d516
Rodney Canlas <47021958+rodneyjdc@users.noreply.github.com>
2022-06-09 14:33:47 -0700

    created the ItemController

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index", this._context.Items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            this._context.Items.Add(item);
+            this._context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Item itemToBeDeleted = this._context.Find<Item>(id);
+            this._context.Remove(itemToBeDeleted);
+            this._context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
